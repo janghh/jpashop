ab20c05b793f89e3e11e4f77ab06539f72c2bfa3
pf3-janghh <janghh@ex-em.com>
2023-05-29 15:45:38 +0900

    [실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화] 섹션 4. API 개발 고급 - 컬렉션 조회 최적화 > 주문 조회 V1: 엔티티 직접 노출

---
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,51 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderSearch;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * V1. 엔티티 직접 노출
+ * - 엔티티가 변하면 API 스펙이 변한다.
+ * - 트랜잭션 안에서 지연 로딩 필요
+ * - 양방향 연관관계 문제
+ *
+ * V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용X)
+ * - 트랜잭션 안에서 지연 로딩 필요
+ * V3. 엔티티를 조회해서 DTO로 변환(fetch join 사용O)
+ * - 페이징 시에는 N 부분을 포기해야함(대신에 batch fetch size? 옵션 주면 N -> 1 쿼리로 변경
+ 가능)
+ *
+ * V4. JPA에서 DTO로 바로 조회, 컬렉션 N 조회 (1 + N Query)
+ * - 페이징 가능
+ * V5. JPA에서 DTO로 바로 조회, 컬렉션 1 조회 최적화 버전 (1 + 1 Query)
+ * - 페이징 가능
+ * V6. JPA에서 DTO로 바로 조회, 플랫 데이터(1Query) (1 Query)
+ * - 페이징 불가능...
+ *
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName(); //Lazy 강제 초기화
+            order.getDelivery().getAddress(); //Lazy 강제 초기화
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o -> o.getItem().getName()); //Lazy 강제 초기화
+        }
+        return all;
+    }
+
+}
